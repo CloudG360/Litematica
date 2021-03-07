7a9d053101982ad0776ff559f6b9c027d2e11fa1
Will <willscullythewag@gmail.com>
2021-03-07 23:09:50 +0000

    Revert "Remove fingerprint violation warning lmao"
    This reverts commit 413ee5f1

--- src/main/java/fi/dy/masa/litematica/Litematica.java
+++ src/main/java/fi/dy/masa/litematica/Litematica.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import fi.dy.masa.litematica.event.RenderHandler;
 import fi.dy.masa.malilib.event.InitializationHandler;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION,
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, certificateFingerprint = Reference.FINGERPRINT,
     guiFactory = "fi.dy.masa.litematica.config.gui.LitematicaGuiFactory",
     updateJSON = "https://raw.githubusercontent.com/maruohon/litematica/forge_1.12.2/update.json",
     clientSideOnly=true, acceptedMinecraftVersions = "1.12.2", dependencies = "required-after:malilib;")
@@ -30,6 +30,19 @@ public class Litematica
     @Mod.EventHandler
     public void onFingerPrintViolation(FMLFingerprintViolationEvent event)
     {
-        // Why was there ever tamper protection here in the first place?
+        // Not running in a dev environment
+        if (event.isDirectory() == false)
+        {
+            logger.warn("*********************************************************************************************");
+            logger.warn("*****                                    WARNING                                        *****");
+            logger.warn("*****                                                                                   *****");
+            logger.warn("*****   The signature of the mod file '{}' does not match the expected fingerprint!     *****", event.getSource().getName());
+            logger.warn("*****   This might mean that the mod file has been tampered with!                       *****");
+            logger.warn("*****   If you did not download the mod {} directly from Curse/CurseForge,       *****", Reference.MOD_NAME);
+            logger.warn("*****   or using one of the well known launchers, and you did not                       *****");
+            logger.warn("*****   modify the mod file at all yourself, then it's possible,                        *****");
+            logger.warn("*****   that it may contain malware or other unwanted things!                           *****");
+            logger.warn("*********************************************************************************************");
+        }
     }
 }
